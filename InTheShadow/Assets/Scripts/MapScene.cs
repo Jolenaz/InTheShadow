4289fde68dbae54718827a88c0b67429ddc6125a
Jolenaz <jonasbellessa@gmail.com>
2018-10-14 14:30:01 +0200

    ajout truc

---
+++ InTheShadow/Assets/Scripts/MapScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScene : MonoBehaviour {
+
+
+    [SerializeField]
+    private spotInput spot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnEnable()
+    {
+        spot.isMovable = false;
+    }
+
+    private void OnDisable()
+    {
+        spot.isMovable = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
